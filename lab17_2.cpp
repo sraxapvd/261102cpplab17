c9bac9802182cc3238e5fe363590d1a57fdef019
Rada <sirada_dee@cmu.ac.th>
2026-02-09 21:44:05 +0700

    lab17_2.cpp completed

--- lab17_2.cpp
+++ lab17_2.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 using namespace std;
 
-void myString(char* &p , int N){
+void myString(char* &p , int N)
+{
 	p = new char[N+1];    
 	for(int i = 0; i < N;i++) p[i] = 'A'+i;
 	p[N] = '\0';
